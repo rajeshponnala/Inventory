bc926e1c927c5cad77ef4358c9b0b61737705d4e
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-03-03 18:55:00 +0530

    return products list

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -1,10 +1,19 @@
 package controllers
 import play.api.mvc._
+import play.api.Play._
+import Tables._
+import models._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import Tables.profile.simple._
 
 object Products extends Controller {
 
+  implicit val productWrites = new Writes[Tables.ProductsRow] {
+     def writes(p: Tables.ProductsRow) = Json.obj("id" -> p.id,"Name" -> p.name,"Description"->p.description)
+  }
   def products = Action{
-     Ok("HelloWorld")
+    Ok(Json.toJson(Product.getProducts()))
   }
 
 }
