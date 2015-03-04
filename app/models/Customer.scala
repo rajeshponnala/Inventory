ea5e7707b7ca4129d15e1cb7ae90b3f886fb73c2
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-03-04 14:03:45 +0530

    rename function insert to create

--- app/models/Customer.scala
+++ app/models/Customer.scala
@@ -12,7 +12,7 @@ object Customer {
       SQL("select * From Inventory.Customers").map { case Row(id: Int,name: String,address: String,Some(email: String),phone: String) => Tables.CustomersRow(id,name,address,Some(email),phone) }.list
   }
 
-  def Insert(customer: Tables.CustomersRow) : Option[Long]= DB.withConnection{ implicit c =>
+  def create(customer: Tables.CustomersRow) : Option[Long]= DB.withConnection{ implicit c =>
         SQL("insert into Inventory.Customers(Name,Address,Email,Phone) values({name},{address},{email},{phone})")
                             .on('name-> customer.name,'address->customer.address,'email-> customer.email.getOrElse(""),'phone->customer.phone)
                             .executeInsert()
