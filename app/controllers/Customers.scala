26aded8fa9f7581c7f88ebde68d99280f27ec210
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-03-04 13:53:24 +0530

    remove custom json serializers;add Json macro serializers

--- app/controllers/Customers.scala
+++ app/controllers/Customers.scala
@@ -1,40 +1,25 @@
-
 package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import models.Customer._
-import Tables._
+import Tables.Tables._
 import play.api.libs.functional.syntax._
 
 
 object Customers extends Controller {
 
- implicit val customerWrites = new Writes[Tables.CustomersRow] {
-  def writes(p: Tables.CustomersRow) = Json.obj(
-     "id" -> p.id,
-    "name" -> p.name,
-    "address"-> p.address,
-    "email" -> p.email,
-    "phone" -> p.phone
-   )
- }
- 
-  implicit val customerReads: Reads[Tables.CustomersRow] = (
-  (JsPath \ "id").read[Int] and
-  (JsPath \ "name").read[String] and
-  (JsPath \ "address").read[String] and
-  (JsPath \ "email").readNullable[String] and
-  (JsPath \ "phone").read[String] 
-  )(Tables.CustomersRow.apply _)
+  implicit val customerReads = Json.reads[CustomersRow] 
+  implicit val customerWrites = Json.writes[CustomersRow]
 
- 
+
   def customers = Action {
     Ok(Json.toJson(getAll()))
+   // Ok("hello")
   }
 
   def postCustomer = Action(parse.json) { request =>
-    val result = Insert(request.body.as[Tables.CustomersRow])
+    val result = Insert(request.body.as[CustomersRow])
     result.map(i => Ok(i.toString)).getOrElse(InternalServerError("Error in creating"))
    }
 }
